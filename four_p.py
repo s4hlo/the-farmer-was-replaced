3d60b171d1698c784d396100670ac24532728e5d
s4hlo <rfl.magno02@gmail.com>
2025-10-18 18:51:06 -0300

    up

--- four_p.py
+++ four_p.py
@@ -14,7 +14,7 @@ def remove_if_not_p():
 def square():
 	planting = True
 	size = get_world_size()
-	half = size / 2
+	half = size // 2
 	harvest_pos = [(0,0), (0,half), (half,0), (half,half)]
 
 	has_dead = {
@@ -23,46 +23,43 @@ def square():
 		harvest_pos[2]: True,
 		harvest_pos[3]: True
 	}
-
 	while planting:
 		for x in range(size):
 			for y in range(size):
 				remove_if_not_p()
 				if (x,y) in harvest_pos:
-					do_a_flip()
-					print(has_dead[(x,y)])
 					if has_dead[(x,y)] == False:
 						harvest()
 					has_dead[(x,y)] = False
 					
 				if x < half and y < half:
 					set_soil()
-					if get_entity_type() == Entities.Dead_Pumpkin:
+					if get_entity_type() != Entities.Pumpkin:
 						has_dead[harvest_pos[0]] = True
 					plant(Entities.Pumpkin)
 					
 				if x >= half and y < half:
 					set_soil()
-					if get_entity_type() == Entities.Dead_Pumpkin:
+					if get_entity_type() != Entities.Pumpkin:
 						has_dead[harvest_pos[1]] = True
 					plant(Entities.Pumpkin)
 					
 				if x < half and y >= half:
 					set_soil()
-					if get_entity_type() == Entities.Dead_Pumpkin:
+					if get_entity_type() != Entities.Pumpkin:
 						has_dead[harvest_pos[2]] = True
 					plant(Entities.Pumpkin)
 					
 				if x >= half and y >= half:
 					set_soil()
-					if get_entity_type() == Entities.Dead_Pumpkin:
+					if get_entity_type() != Entities.Pumpkin:
 						has_dead[harvest_pos[3]] = True
 					plant(Entities.Pumpkin)
 			 
 				move(East)
 			move(North)
 
-set_world_size(6)	
+set_world_size(12)	
 while True: 
 	square()      
 		
