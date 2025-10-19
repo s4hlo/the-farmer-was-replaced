f89cc7ee04fac22883adc209a5babb8129856de5
s4hlo <rfl.magno02@gmail.com>
2025-10-19 17:12:44 -0300

    upload

--- cactus_m.py
+++ cactus_m.py
@@ -6,28 +6,13 @@ def condition(orr, opps):
 	else:
 		return measure() < measure(opps)
 
-def sort(dir, size, order='desc'):
-	opps = u.opposite(dir)
-	for i in range(1,size):
-		move(dir)
-		j = i
-		steps_back = 0
- 
-		while j > 0 and condition(order, opps):
-			swap(opps)
-			move(opps)
-			steps_back = steps_back + 1
-			j = j -1
-	
-		for _ in range(steps_back):
-			move(dir)
-	 
-def plant_and_sort(dir, size, order='desc'):
+def sort(dir, size, order, do_plant=False):
 	opps = u.opposite(dir)
 	for i in range(size):
-		if get_ground_type() == Grounds.Grassland:
-			till()
-		plant(Entities.Cactus)
+		if do_plant:
+			if get_ground_type() == Grounds.Grassland:
+				till()
+			plant(Entities.Cactus)
 
 		j = i
 		steps_back = 0
@@ -37,7 +22,6 @@ def plant_and_sort(dir, size, order='desc'):
 			move(opps)    
 			steps_back += 1
 			j -= 1
-	
 		for _ in range(steps_back):
 			move(dir)
 
@@ -48,24 +32,20 @@ clear()
 set_world_size(15)
 size = get_world_size()
 
-
- 
 def plant_sort_north():
-	plant_and_sort(East, size, 'asc')
+	sort(East, size, 'asc', True)
+def sort_north():
+	sort(North, size, 'asc')
  
 for i in range(size):
 	spawn_drone(plant_sort_north)
 	move(North)
 
-def sort_north():
-	sort(North, size, 'asc')
-	
 while num_drones() > 1:
 	pass
 for i in range(size):
 	spawn_drone(sort_north)
 	move(East)
-
 
 while True:
 		do_a_flip()
