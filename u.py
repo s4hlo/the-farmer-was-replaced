8b05046b8ce27b52c5d5d72ddf412aa335ae700d
s4hlo <rfl.magno02@gmail.com>
2025-10-19 15:04:23 -0300

    Add smart_water and go_to_pos functions for improved item management and navigation

--- u.py
+++ u.py
@@ -6,3 +6,20 @@ def smart_water():
 		use_item(Items.Water)
 		if num_items(Items.Water) < 1:
 			break
+ 
+def go_to_pos(xy):
+    x_target, y_target = xy
+    x = get_pos_x()
+    y = get_pos_y()
+    while x < x_target:
+        move(East)
+        x += 1
+    while x > x_target:
+        move(West)
+        x -= 1
+    while y < y_target:
+        move(North)
+        y += 1
+    while y > y_target:
+        move(South)
+        y -= 1
