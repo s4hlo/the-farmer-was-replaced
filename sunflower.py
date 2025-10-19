01b2bd568654b67400a40742e0ab19160c543c1d
s4hlo <rfl.magno02@gmail.com>
2025-10-18 21:21:32 -0300

    up

--- sunflower.py
+++ sunflower.py
@@ -13,8 +13,6 @@ petals_pos = {
 }
 clear()
 
-print(u.get_pos())
-
 alive = 0
 def plant_and_save(alive):
 	if get_ground_type() == Grounds.Grassland:
@@ -25,23 +23,34 @@ def plant_and_save(alive):
 		alive = alive + 1
 	return alive
 
+def harvest_alive(alive, pos):
+	value = measure()
+	lista = petals_pos[value]
+	lista.remove(pos)
+	harvest()
+	alive -= 1
+	return alive
 
 def tentar_colher(alive, pos):
+  # TODO improve this
+	sete = petals_pos[7]
+	if len(sete) > 1 and can_harvest():
+		harvest_alive(alive, pos)
+  
+  
 	for nivel in range(15, 7, -1):
 		lista = petals_pos[nivel]
 		if not lista: 
 			continue
 		if pos in lista and can_harvest():
-			harvest()
-			lista.remove(pos)
-			alive -= 1
+			alive = harvest_alive(alive, pos)
 			alive = plant_and_save(alive)
 		break
 	return alive
 
 
 set_execution_speed(0)
-set_world_size(6)
+set_world_size(4)
 size = get_world_size()
 while True:
 	for i in range(size):
